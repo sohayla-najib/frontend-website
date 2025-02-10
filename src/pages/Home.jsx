850dd61bf0efff074ebd60666b2609a1b27f996f
zeinab <zeinabhteit1@gmail.com>
2025-02-10 22:36:16 +0200

    homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-// import Stats from "../components/Stats";
+
 import Footer from "../components/Footer";
 
 function Home() {
