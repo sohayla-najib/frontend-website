1253a958936a4ab8e816aac2ede318c03fb05a8a
Fatme <fatme34saeed@gmail.com>
2025-02-12 11:31:12 +0200

    Projects page

---
+++ src/pages/projects.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
+import Footer from "../components/Footer";
+
+function Project(){
+    return(
+        <>
+        <Navbar/>
+        <Projects/>
+        <Footer/>
+        </>
+    )
+}
+
+export default Project;
